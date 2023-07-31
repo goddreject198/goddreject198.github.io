35490314d49073c5cc9ea048e6583edc1605caad
goddreject198 <51882251+goddreject198@users.noreply.github.com>
2023-07-31 20:21:25 +0700

    Update hpbd.js

--- hpbd.js
+++ hpbd.js
@@ -1,5 +1,5 @@
-const contentLetterSrart_actived = "Hãy nói gì đó ở đây trước khi người ấy mở bức thư nhé." //Lời mở đầu cho bức thư
-const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn yêu thương." //Nội dung của bức thư
+const contentLetterSrart_actived = "Nghe nói nay sinh nhật bạn mình?" //Lời mở đầu cho bức thư
+const mainContentLetter = "Trông chờ cái tó chi, bố m rảnh thôi. Đồng chí snvv!" //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư

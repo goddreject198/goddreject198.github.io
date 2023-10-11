aab8076835cfe6b6eefde4f5de9c660aac1c415a
goddreject198 <51882251+goddreject198@users.noreply.github.com>
2023-10-11 15:49:41 +0700

    Update hpbd.js

--- hpbd.js
+++ hpbd.js
@@ -1,5 +1,5 @@
-const contentLetterSrart_actived = "Nghe nói nay sinh nhật bạn mình?" //Lời mở đầu cho bức thư
-const mainContentLetter = "Trông chờ cái tó chi, bố m rảnh thôi. Đồng chí sinh nhật vui vẻ!" //Nội dung của bức thư
+const contentLetterSrart_actived = "Happy birthday muộn cho em bé <3" //Lời mở đầu cho bức thư
+const mainContentLetter = "Mãi bên nhao em nhéeee moah moah moah <3" //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
